953fe382a5a4bb306d5a7f000f3b1807f5595184
Lur81 <l.cano.soler@gmail.com>
2023-03-23 14:44:06 +0100

    update

--- src/api/models/movies.model.js
+++ src/api/models/movies.model.js
@@ -8,7 +8,11 @@ const movieSchema = new Schema(
         title: {type: String, required: true},
         director: {type: String, required: true},
         year: {type: Number, required: true},
-        genre: {type: String, required: true}
+        duration: {type: Number, required: true},
+        genre: {type: String, required: true},
+        valoration: {type: Number, required: false},
+        synopsis: {type: String, required: true},
+        billboard: {type: String, required: false, default: "https://res.cloudinary.com/da3lrqri1/image/upload/v1679572491/movies/portadaDefault_n65yft.jpg"},
     },{
         timestamps: true
     }
