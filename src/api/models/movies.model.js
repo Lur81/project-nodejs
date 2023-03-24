d484229d687ccac8fe47a0599e9d42bd3794f72e
Lur81 <l.cano.soler@gmail.com>
2023-03-24 15:16:31 +0100

    update

--- src/api/models/movies.model.js
+++ src/api/models/movies.model.js
@@ -5,13 +5,13 @@ const Schema = mongoose.Schema;
 const movieSchema = new Schema(
     {
         title: {type: String, required: true},
-        director: {type: String, required: true},
-        year: {type: Number, required: true},
-        duration: {type: Number, required: true},
-        genre: {type: String, required: true},
+        director: {type: String, required: false},
+        year: {type: Number, required: false},
+        duration: {type: Number, required: false},
+        genre: {type: String, required: false},
         valoration: {type: Number, required: false},
         synopsis: {type: String, required: false},
-        billboard: {type: String, required: true, default: "https://res.cloudinary.com/da3lrqri1/image/upload/v1679572491/movies/portadaDefault_n65yft.jpg"},
+        billboard: {type: String, required: false, default: "https://res.cloudinary.com/da3lrqri1/image/upload/v1679572491/movies/portadaDefault_n65yft.jpg"},
     },{
         timestamps: true
     }
