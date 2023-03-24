918e3c725750cbdff5cb5ac25aafec401b817517
Lur81 <l.cano.soler@gmail.com>
2023-03-24 19:17:01 +0100

    update complete

--- src/seeds/user.seed.js
+++ src/seeds/user.seed.js
@@ -7,31 +7,31 @@ const arrayUsers = [
 {
     "name": "lourdes",
     "email": "lourdes@lourdes.com",
-    "password": "lourdes12345",
+    "password": "Lourdes12345",
     "membership": 1,
     "postalCode": "28007",
-    "rol": "admin"
+    "role": "admin"
 },{
     "name": "ruben",
     "email": "ruben@ruben.com",
-    "password": "ruben12345",
+    "password": "Ruben12345",
     "membership": 2,
     "postalCode": "22389",
-    "rol": "admin"
+    "role": "admin"
 },{
     "name": "nico",
     "email": "nico@nico.com",
-    "password": "nico12345",
+    "password": "Nico12345",
     "membership": 3,
     "postalCode": "24567",
-    "rol": "admin"
+    "role": "admin"
 },{
     "name": "daniel",
     "email": "daniel@daniel.com",
-    "password": "daniel12345",
+    "password": "Daniel12345",
     "membership": 4,
     "postalCode": "28223",
-    "rol": "user"
+    "role": "user"
 },
 ];
 
